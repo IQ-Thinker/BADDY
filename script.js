a78d750b2442c570fc8094a8211b869772a0d778
Robbie <79983105+IQ-Thinker@users.noreply.github.com>
2023-02-01 16:07:32 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+abc() {
+  window.open("https://ixl.com/signin/hopkinton");
+  window.open("https://clever.com/in/hopkinton-public-schools/student/portal");
+  window.open("https://hopkinton.schoology.com/")
+}
